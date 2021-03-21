2c741e964ecc2b179d761b82040241aadc4054d0
chadcomiter <chadcomiter@gmail.com>
2021-03-21 13:10:48 -0400

    Added Heroku Server URL

--- src/services/widget-service.js
+++ src/services/widget-service.js
@@ -1,6 +1,6 @@
 export const createWidgetForTopic = (topicId) => 
 
-    fetch(`http://localhost:8080/api/topics/${topicId}/widgets`, {
+    fetch(`https://whiteboard-ccomiter-server.herokuapp.com/api/topics/${topicId}/widgets`, {
         method: "POST",
         body: JSON.stringify({type: "HEADING", size: 1, text: "New Widget"}),
         headers: {
@@ -13,22 +13,22 @@ export const createWidgetForTopic = (topicId) =>
         })
         */
 export const findAllWidgets = () =>
-    fetch(`http://localhost:8080/api/widgets`)
+    fetch(`https://whiteboard-ccomiter-server.herokuapp.com/api/widgets`)
         .then(response => response.json())
 
 export const findWidgetsForTopic = (topicId) => 
-    fetch(`http://localhost:8080/api/topics/${topicId}/widgets`)
+    fetch(`https://whiteboard-ccomiter-server.herokuapp.com/api/topics/${topicId}/widgets`)
         .then(response => response.json())
         
 export const deleteWidget = (wid) =>
-    fetch(`http://localhost:8080/api/widgets/${wid}`, {
+    fetch(`https://whiteboard-ccomiter-server.herokuapp.com/api/widgets/${wid}`, {
         method: "DELETE",
     })
         .then(response => response.json())/*{
         setWidgets((widgets) => widgets.filter(w => w.id !== wid))
     })*/
 export const updateWidget = (wid, widget) =>
-    fetch(`http://localhost:8080/api/widgets/${wid}`, {
+    fetch(`https://whiteboard-ccomiter-server.herokuapp.com/api/widgets/${wid}`, {
         method: "PUT",
         body: JSON.stringify(widget),
         headers: {
