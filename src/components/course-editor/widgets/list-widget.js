c0f0b8e8dcb90d2b39d5705f545dcdef44a656df
chadcomiter <chadcomiter@gmail.com>
2021-04-02 14:32:13 -0400

    formatting

--- src/components/course-editor/widgets/list-widget.js
+++ src/components/course-editor/widgets/list-widget.js
@@ -1,64 +1,70 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const ListWidget = ({ widget, updateWidget, deleteWidget} ) => {
-    const [cachedWidget, setCachedWidget] = useState(widget)
-    const [editing, setEditing] = useState(false)
-    return (
+const ListWidget = ({ widget, updateWidget, deleteWidget }) => {
+  const [cachedWidget, setCachedWidget] = useState(widget);
+  const [editing, setEditing] = useState(false);
+  return (
+    <>
+      {editing && (
         <>
-            {
-                editing && 
-                <>
-                    <i onClick={() => {
-                        updateWidget(widget.id, cachedWidget)
-                        setEditing(false)
-                    }} className="fas fa-check float-right"></i>
-                    <i onClick={() => {
-                        deleteWidget(widget.id)
-                        setEditing(false)
-                    }} className="fas fa-b thean float-right"></i>
-                    <input type="checkbox" checked={cachedWidget.ordered}
-                        onChange={(e) => setCachedWidget({...cachedWidget, ordered: e.target.checked})}/> Check To Order
-                    <textarea
-                        value={cachedWidget.text} className="form-control"
-                        rows="5"
-                        onChange={(e) => 
-                            setCachedWidget({
-                                ...cachedWidget, ordered: e.target.checked
-                            })}>
-                    </textarea>
-                </>
+          <i
+            onClick={() => {
+              updateWidget(widget.id, cachedWidget);
+              setEditing(false);
+            }}
+            className="fas fa-check float-right"
+          ></i>
+          <i
+            onClick={() => {
+              deleteWidget(widget.id);
+              setEditing(false);
+            }}
+            className="fas fa-b thean float-right"
+          ></i>
+          <input
+            type="checkbox"
+            checked={cachedWidget.ordered}
+            onChange={(e) =>
+              setCachedWidget({ ...cachedWidget, ordered: e.target.checked })
             }
-            {
-                !editing && 
-                <>
-                    <i onClick={() => setEditing(true)} className="fas fa-edit float-right"></i>
-                    {
-                        cachedWidget.ordered && 
-                            <ol>
-                                {cachedWidget.text.split("\n").map((component) => {
-                                    return (
-                                        <li>
-                                            component
-                                        </li>
-                                    )
-                                })}
-                            </ol>
-                    }
-                    {
-                        !widget.ordered && 
-                        <ul>
-                            {cachedWidget.text.split("\n").map((component) => {
-                                return (
-                                    <li>
-                                        {component}
-                                    </li>
-                                )
-                            })}
-                        </ul>
-                    }
-                </>
+          />{" "}
+          Check To Order
+          <textarea
+            value={cachedWidget.text}
+            className="form-control"
+            rows="5"
+            onChange={(e) =>
+              setCachedWidget({
+                ...cachedWidget,
+                ordered: e.target.checked,
+              })
             }
+          ></textarea>
         </>
-    )
-}
-export default ListWidget
+      )}
+      {!editing && (
+        <>
+          <i
+            onClick={() => setEditing(true)}
+            className="fas fa-edit float-right"
+          ></i>
+          {cachedWidget.ordered && (
+            <ol>
+              {cachedWidget.text.split("\n").map((component) => {
+                return <li>component</li>;
+              })}
+            </ol>
+          )}
+          {!widget.ordered && (
+            <ul>
+              {cachedWidget.text.split("\n").map((component) => {
+                return <li>{component}</li>;
+              })}
+            </ul>
+          )}
+        </>
+      )}
+    </>
+  );
+};
+export default ListWidget;
